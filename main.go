a5fef2a6b7bc56c1a98c7a9abc371ed38d099b71
huangj229 <286233051@qq.com>
2020-07-08 09:24:58 +0800

    slice usage

--- main.go
+++ main.go
@@ -3,5 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("hello world")
+	slice := []int{1,2,3}
+	fmt.Println("slice:", slice)
+	slice = append(slice, 4)
+	fmt.Println("slice:", slice)
 }
