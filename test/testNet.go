6911448a6fd7642df4035480ca6924031957d681
huangj229 <286233051@qq.com>
2020-07-12 01:24:41 +0800

    net-usage 0.2

--- test/testNet.go
+++ test/testNet.go
@@ -2,14 +2,36 @@ package test
 
 import (
     "fmt"
-	"net"    
+	"net"
+	"bufio"    
 )
 
+
+
 func TestNet() {
+	// resolve IP of baidu.com
 	ip,err := net.ResolveIPAddr("ip", "www.baidu.com")
 	if err != nil {
 		fmt.Println(err)
 		return 
 	}
 	fmt.Println("www.baidu.com: ", ip)
+
+
+    //build connection to baidu.com
+	conn,err := net.Dial("tcp", "baidu.com:80")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	//ask for response from baidu.com
+	fmt.Fprintf(conn, "GET / HTTP/1.0\r\n\r\n")
+	status, err := bufio.NewReader(conn).ReadString('\n')
+	if err != nil {
+		fmt.Println("err:", err)
+		return
+	}
+	fmt.Println("status:", status)
+
+
 }
